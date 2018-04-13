2f9246b371c7addd55dfe838b489b7346b56b5f4
John Kovachi <johnkovachi@Johns-Mac.local>
2018-04-13 16:15:56 -0700

    Add boilerplate express code

--- server.js
+++ server.js
@@ -1,1 +1,21 @@
-var app = require('express');
+console.log('hi');
+
+var express = require('express');
+
+var app = express();
+
+app.use(express.json());
+
+app.listen(4000);
+
+app.get('/', (req, res) => {
+  res.send('hi');
+})
+
+console.log('Listening on port 4000');
+
+/**
+ * Exports express
+ * @public
+ */
+module.exports = app;
