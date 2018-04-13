5a3b60de1eccb137f6504d5c9b14d587c57d95ba
John Kovachi <johnkovachi@Johns-Mac.local>
2018-04-13 16:34:13 -0700

    Update readme & add dotenv

--- README.md
+++ README.md
@@ -2,3 +2,8 @@
 A Node/Express backend for my project, NightOut.
 
 I used Node because I am familiar with it and it is super easy to set up and deploy to Heroku.
+
+
+# TO-DO
+
+1. Implement the Places API text search. Will be used for centering our map on the app.
--- server.js
+++ server.js
@@ -1,8 +1,6 @@
-console.log('hi');
-
 var express = require('express');
-
 var app = express();
+const dotenv = require('dotenv');
 
 app.use(express.json());
 
@@ -10,6 +8,8 @@ app.listen(4000);
 
 app.get('/', (req, res) => {
   res.send('hi');
+  // Use the key for later.
+  var key = process.env.PLACES_API_KEY;
 })
 
 console.log('Listening on port 4000');
