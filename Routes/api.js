aab473ebbdcac465a7275949070ca6864ecc140c
John Kovachi <johnkovachi@Johns-Mac.local>
2018-04-14 17:51:56 -0700

    Add console log in request

--- Routes/api.js
+++ Routes/api.js
@@ -8,6 +8,7 @@ We take the location specified in req parameters and use that to query the
 Google Places API for our desired location.
 */
 router.post('/places', (req,res) => {
+  console.log(req);
   pl.placesQuery(req.body.location.split(' ').join('%20')).then(result => {
     res.send(JSON.stringify(result.results[0].geometry.location));
   })
